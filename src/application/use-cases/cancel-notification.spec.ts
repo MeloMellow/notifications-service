05cdee2bb4802781d3dc549d51692513c32bfc2e
Melo <melotbhc@gmail.com>
2023-01-09 20:12:12 -0300

    preparing to add git repository

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -1,12 +1,12 @@
 import { inMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
-import { SendNotification } from './send-notification';
+import { CancelNotification } from './cancel-notification';
 
-describe('Send notification', () => {
+describe('Cancel notification', () => {
   it('sould be able to send a notification', async () => {
     const notificationsRepository = new inMemoryNotificationsRepository();
-    const sendNotification = new SendNotification(notificationsRepository);
+    const cancelNotification = new CancelNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    const { notification } = await cancelNotification.execute({
       content: 'This is a notification',
       category: 'social',
       recipientId: 'example-recipient-id',
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,9 +1,15 @@
-import { Notification } from '../../src/application/entities/notification';
-import { NotificationsRepository } from '../../src/application/repositories/notifications-repository';
+import { Notification } from '@application/entities/notification';
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
 
 export class inMemoryNotificationsRepository
   implements NotificationsRepository
 {
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
   public notifications: Notification[] = [];
 
   async create(notification: Notification): Promise<void> {
